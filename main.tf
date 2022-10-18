0a2a3c3ec0f91ad3c686684d64798b17c6a6e163
sirharis214 <hariskido214@gmail.com>
2022-10-17 21:21:52 -0400

    s3 bucket tf-apply

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = ">= 1.1.7"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = ">= 3.0"
-        }
+  required_version = ">= 1.1.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
     }
+  }
 }
